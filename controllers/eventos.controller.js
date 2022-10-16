b9719cdf59b0ca9232120867a654de778d0e3a8d
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-10-16 00:23:41 -0500

    Creación de un evento con su validación

--- controllers/eventos.controller.js
+++ controllers/eventos.controller.js
@@ -9,7 +9,31 @@ const obtenerEventos = async (req, res = response) => {
   res.json(eventos);
 }
 
+const crearEvento = async (req, res = response) => {
+
+  const { nombre, fecha } = req.body;
+
+  const eventoDB = await Evento.findOne({ nombre });
+
+  if (eventoDB) {
+    return res.status(400).json({
+      msg: `El Evento ${categoriaDB.nombre}, ya existe.`
+    })
+  }
+
+  const data = {
+    nombre,
+    fecha
+  }
+
+  const evento = new Evento(data);
+
+  await evento.save();
+
+  res.status(201).json(evento);
+}
 
 module.exports = {
-  obtenerEventos
+  obtenerEventos,
+  crearEvento
 }
--- routes/eventos.routes.js
+++ routes/eventos.routes.js
@@ -1,9 +1,17 @@
-const { Router, response } = require('express');
-const { obtenerEventos } = require('../controllers/eventos.controller');
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { obtenerEventos, crearEvento } = require('../controllers/eventos.controller');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.get('/', obtenerEventos);
+
+router.post('/', [
+  check('nombre', 'El Nombre es obligatorio').not().isEmpty(),
+  check('fecha', 'La Fecha es obligatoria').not().isEmpty(),
+  validarCampos
+], crearEvento);
 
 
 module.exports = router;
