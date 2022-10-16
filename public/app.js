b8c8f23e0e0ccc05e9e7960cf146e07ff1f986b3
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-10-16 02:56:58 -0500

    Lista de eventos que coinciden con la búsqueda

--- public/app.js
+++ public/app.js
@@ -1,9 +1,9 @@
-
 const inputNombre = document.getElementById('nombre_evento');
 const inputFecha = document.getElementById('fecha_evento');
 const inputAsistencia = document.getElementById('asistencia_evento');
 const inputBuscar = document.getElementById('buscar_evento');
 const formBuscar = document.getElementById('form');
+const listaCoindicencias = document.getElementById('coincidencias');
 const tablaBody = document.getElementById('tbody');
 
 const obtenerEvento = async (termino = 'a') => {
@@ -15,25 +15,36 @@ const obtenerEvento = async (termino = 'a') => {
   inputFecha.value = results_eventos[0].evento.fecha;
   inputAsistencia.value = results_eventos[0].evento.asistencia;
 
-  let html = '';
+  let htmlLi = ''
+  results_eventos.map(({ evento }) => {
+
+    htmlLi +=
+      `
+      <li class="list-group-item">${evento.nombre}</li>
+    `
+  });
+
+  listaCoindicencias.innerHTML = htmlLi;
+
+  let htmlTr = '';
   let index = 0;
 
   results_asistentes.map(({ asistente }) => {
 
     const horaIngreso = new Date(asistente.registro);
 
-    html +=
+    htmlTr +=
       `
       <tr class="text-center">
         <td>${++index}</td>
-        <th>${asistente.nombre}</th>
+        <td>${asistente.nombre}</td>
         <td>${asistente.matricula}</td>
         <td>${horaIngreso.toLocaleTimeString()}</td>
       </tr>
     `;
   });
 
-  tablaBody.innerHTML = html;
+  tablaBody.innerHTML = htmlTr;
 
 }
 
