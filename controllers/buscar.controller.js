9c072c97d1aed58c7da8b4821d61b12370ed8ba0
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-10-16 01:46:48 -0500

    Obtención de los ids que corresponden a la búsqueda de nombre de evento

--- controllers/buscar.controller.js
+++ controllers/buscar.controller.js
@@ -3,3 +3,27 @@ const { ObjectId } = require("mongoose").Types;
 
 const Evento = require('../models/evento');
 
+const buscar = async (req, res = response) => {
+
+  const { evento } = req.params;
+
+  const regex = new RegExp(evento, 'i');
+
+  const eventos = await Evento.find({ nombre: regex });
+
+  let idEventos = []
+  eventos.map((evento) => {
+    idEventos.push(evento._id.toString());
+  });
+  console.log(idEventos);
+
+  res.json({
+    results_eventos: eventos,
+    results_asistentes: eventos,
+  })
+}
+
+
+module.exports = {
+  buscar
+};
